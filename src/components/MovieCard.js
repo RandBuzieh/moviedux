9abf77f9ed479adcbfe3e260feea670340cebe6a
RandBuzieh <randbuzieh@gmail.com>
2025-04-10 18:08:22 +0400

    first commit

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,7 +3,7 @@ import "../styles.css";
 
 export default function MovieCard({ movie, isWatchlisted, toggleWatchlist }) {
   const handelImgError = (e) => {
-    e.target.src = "./moviedux/images/default.jpg";
+    e.target.src = "/images/default.jpg";
   };
   const rating = (rate) => {
     if (movie.rating >= 8) return "rating-good";
@@ -13,7 +13,7 @@ export default function MovieCard({ movie, isWatchlisted, toggleWatchlist }) {
   return (
     <div key={movie.id} className="movie-card">
       <img
-        src={`./moviedux/images/${movie.image}`}
+        src={`/images/${movie.image}`}
         alt={movie.title}
         onError={handelImgError}
       ></img>
